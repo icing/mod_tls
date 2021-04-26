f9f7f192188051bf6f9eba74cef9c08e900ebbc2
Stefan Eissing <stefan.eissing@greenbytes.de>
2021-04-26 15:47:22 +0200

    * adjusting call to new rustls_all_ciphersuites_get_entry method name.

--- src/tls_proto.c
+++ src/tls_proto.c
@@ -548,7 +548,7 @@ tls_proto_conf_t *tls_proto_init(apr_pool_t *pool, server_rec *s)
     conf->supported_cipher_ids = apr_array_make(pool, 10, sizeof(apr_uint16_t));
     conf->rustls_ciphers_by_id = apr_hash_make(pool);
     i = 0;
-    while ((rustls_suite = rustls_all_ciphersuites_get(i++))) {
+    while ((rustls_suite = rustls_all_ciphersuites_get_entry(i++))) {
         id = rustls_supported_ciphersuite_get_suite(rustls_suite);
         rcipher = apr_pcalloc(pool, sizeof(*rcipher));
         rcipher->id = id;
