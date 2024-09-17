a89e93c515790bdb1d6246cbda0519241e6495c4
Stefan Eissing <stefan@eissing.org>
2024-09-17 12:14:02 +0200

    v0.13.0

--- src/tls_version.h
+++ src/tls_version.h
@@ -26,7 +26,7 @@
  * @macro
  * Version number of the md module as c string
  */
-#define MOD_TLS_VERSION "0.8.3-git"
+#define MOD_TLS_VERSION "0.13.0-git"
 
 /**
  * @macro
@@ -34,6 +34,6 @@
  * release. This is a 24 bit number with 8 bits for major number, 8 bits
  * for minor and 8 bits for patch. Version 1.2.3 becomes 0x010203.
  */
-#define MOD_TLS_VERSION_NUM 0x000803
+#define MOD_TLS_VERSION_NUM 0x000d00
 
 #endif /* mod_md_md_version_h */
